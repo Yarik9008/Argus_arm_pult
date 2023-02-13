54e2bb7ab5640a154e3e2b06e6ce103f293323ad
Yarik9008 <iaroslav-muhatdinov@yandex.ru>
2023-02-14 00:52:46 +0300

    e

---
+++ dop_Argus_arm_pult/src/Config.h
@@ -0,0 +1,16 @@
+
+const uint16_t PIN_BUT0 = 0;
+const uint16_t PIN_BUT1 = 1;
+const uint16_t PIN_BUT2 = 2;
+const uint16_t PIN_BUT3 = 3;
+const uint16_t PIN_BUT4 = 4;
+const uint16_t PIN_BUT5 = 5;
+const uint16_t PIN_BUT6 = 6;
+const uint16_t PIN_BUT7 = 7;
+const uint16_t PIN_BUT8 = 8;
+const uint16_t PIN_BUT9 = 9;
+
+
+int ButtonState[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+String OutData = "$";
+
